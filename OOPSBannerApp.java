fb6816a5a5ce21036c2912ac29b19f85a89eb3c2
Shubh Kumar Pathak <shubhpathak660@gmail.com>
2026-03-02 22:58:36 +0530

    UC6: Refactor using static functions

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,26 +1,35 @@
 /**
  * OOPSBannerApp
  *
- * UC5: Array Initialization using String.join()
+ * UC6: Using Static Functions
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Array declaration + initialization together
-        String[] banner = new String[] {
-            String.join("   ", "*****", "*****", "*****", "*****"),
-            String.join("   ", "*   *", "*   *", "*   *", "*     "),
-            String.join("   ", "*   *", "*   *", "*   *", "*     "),
-            String.join("   ", "*****", "*****", "*****", "*****"),
-            String.join("   ", "*     ", "*     ", "*     ", "    *"),
-            String.join("   ", "*     ", "*     ", "*     ", "    *"),
-            String.join("   ", "*     ", "*     ", "*     ", "*****")
-        };
+        String[] banner = buildBanner();
 
-        // Enhanced for-loop
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    // Static function to build banner
+    public static String[] buildBanner() {
+
+        return new String[] {
+            buildLine("*****", "*****", "*****", "*****"),
+            buildLine("*   *", "*   *", "*   *", "*     "),
+            buildLine("*   *", "*   *", "*   *", "*     "),
+            buildLine("*****", "*****", "*****", "*****"),
+            buildLine("*     ", "*     ", "*     ", "    *"),
+            buildLine("*     ", "*     ", "*     ", "    *"),
+            buildLine("*     ", "*     ", "*     ", "*****")
+        };
+    }
+
+    // Static helper function
+    public static String buildLine(String a, String b, String c, String d) {
+        return String.join("   ", a, b, c, d);
     }
 }
