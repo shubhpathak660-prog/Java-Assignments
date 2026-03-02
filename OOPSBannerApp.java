0be10e87b11a960a8d9b640dbc4ffa7becbb9f61
Shubh Kumar Pathak <shubhpathak660@gmail.com>
2026-03-02 21:27:01 +0530

    UC1: Implement Print OOPS functionality

---
+++ OOPSBannerApp.java
@@ -0,0 +1,11 @@
+/**
+ * OOPSBannerApp
+ *
+ * UC1: Print the literal text "OOPS" to console.
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
